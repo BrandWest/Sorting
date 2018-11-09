e37ba4cc4821afe7b85ccd95449b361d4b14cb9b
Brandon West <43622851+BrandWest@users.noreply.github.com>
2018-11-09 13:47:05 -0500

    Update Assignment2_Brennen_West_Sorting_Part1.c

--- Assignment2_Brennen_West_Sorting_Part1.c
+++ Assignment2_Brennen_West_Sorting_Part1.c
@@ -1,6 +1,6 @@
 /*
 ====================================================
-Author: Brandon West, Christopher Brennen
+Author: Brandon West
 Class: PROG 20025
 Assignemnt 2: Fun with Sorting!
 Date: Oct 19, 2018
@@ -18,23 +18,27 @@ void insertionSort (FILE *filePtr, int inplaceInsertArray[], int inputSize );
 
 int main()
 {
+  // variables 
   int inputSize = 0, counter = 1;
 
+  // file pointers to look in the file specified and write to the file specified
   FILE *filePtr, *tempPtr;
   filePtr = fopen ("AlmostSorted.txt", "r" );
   FILE *newPtr;
   newPtr = fopen ( "InsertionSorted.txt", "w" );
-
+  
+  // variables to hold the max and min values of the insert sort
   int max = 0;
   int min = 0;
-  // if the file fails to open
+  // if the file fails to open closes filePtr and exits the program
   if( filePtr == NULL )
   {
     printf ( "Error opening file" );
     fclose(filePtr);
     exit(1);
   }
-  // else if the file is successful
+  
+  // else the file is successful
   else
   {
     printf( "Enter inputSize: " );
@@ -42,22 +46,26 @@ int main()
     // inputSize = 200000; // input siez
     int inplaceInsertArray[ inputSize ]; // array size
     counter = 1;
-
+    // tracks time for the sort
     clock_t start = clock();
+    // calls the insert sort method with filePtr, array and inputsize as arguments
     insertionSort ( filePtr, inplaceInsertArray, inputSize );
+    // calculates the time taken from clock start to finish
     double timeElapsed = ( (double) clock () - start ) / CLOCKS_PER_SEC;
     printf ( "The %d run at %d is %2.f\n", counter, inputSize, timeElapsed );
-
+    // writes sorted list to the file
     for ( int index = 0; index < inputSize; index++ )
     {
         fprintf ( newPtr, "%d\n", inplaceInsertArray [ index ] );
     }
-  }
-
+  }// end of  else
+  
+  // clsoes both file pointers
   fclose ( filePtr );
   fclose ( newPtr );
   return 0;
-}
+}// end of main
+
 // function to perform the insertion sort
 void insertionSort (FILE *filePtr, int inplaceInsertArray[], int inputSize )
 {
@@ -85,4 +93,4 @@ void insertionSort (FILE *filePtr, int inplaceInsertArray[], int inputSize )
     // set the counter variable to temp1.
     inplaceInsertArray [ counter + 1  ] = temp1;
   }// end of for loop
-}
+} // end of insertionSort function
